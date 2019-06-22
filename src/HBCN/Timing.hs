93ec4926bdfa45550a42457f1603461ca77abea2
Marcos Sartori <msartori@thesiriusproject.com>
2019-06-22 12:52:02 -0300

    Corrected maxdelay computation

--- src/HBCN/Timing.hs
+++ src/HBCN/Timing.hs
@@ -65,7 +65,7 @@ arrivalTimeEq cycleTime minDelay biasing (place, src, dst) = do
 constraintCycleTime :: HBCN -> Double -> Double -> Double -> TimingLP
 constraintCycleTime hbcn cycleTime minDelay biasing = execLPM $ do
   setDirection Max
-  setObjective (linCombination [(1, DelayFactor), (1, PseudoClock)])
+  setObjective (linCombination [(1, DelayFactor)])
   setVarBounds DelayFactor $ LBound 0
   setVarBounds PseudoClock $ LBound 0
   mapM_ (arrivalTimeEq cycleTime minDelay biasing) $ edgeList hbcn
